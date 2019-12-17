ec0a9069e0aaa444db402438dc5e9fa59633cb1d
MidgeWilliams <43554894+MidgeWilliams@users.noreply.github.com>
2019-12-17 15:49:48 -0500

    adding a summary to README and making my main.py the only main.py

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# Ling131_FinalProject" 
+"# Ling131_FinalProject"
+Ben Siege and Midgie Williams
+
+in main:
+Trains a part of speech tagger on the Brown News Corpus.
+Uses that tagger to tag the desired input.
+Then uses the input and tagger to create a haiku with (extremely) loosely accurate syntax
+
+in haiku:
+estimates the number of syllables in a word using vowel information and some (very) simple phonology
+then uses the parts of speech and syllable counts for each word to create a three line poem, where the first line has 5 syllables, the second 7 and the third 5.
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-# import argparse
-# import os
-# Didn't seem to actually need the imports above
+"""
+Main file to run.
+
+"""
 import re
 import nltk
-import cmudict
 import haiku
-from syllables import estimate
 from nltk.corpus import PlaintextCorpusReader
 from nltk.corpus import brown as n_brown
 from nltk.corpus import wordnet as wn
 from postag import Tagger
 
 STOPLIST = set(nltk.corpus.stopwords.words())
-cdict = cmudict.dict()
 
 def is_content_word(word):
     """A content word is not on the stoplist and its first character is a letter."""
@@ -28,46 +26,17 @@ def tagger_train(a):
     sents_train = sents[0:int(len(sents) * .9)]
     a.train(sents_train)
 
-def estimator(word):
-    cword = cdict.get(word,None)
-    if cword:
-        cmudict_syllables = 0
-        for phone in cword[0]:
-            if re.match(r"\w*[012]$", phone):
-                cmudict_syllables += 1
-        return cmudict_syllables
-    return estimate(word)
-
-def cat(wordL,tagger):
-    tags = tagger.tag(wordL)
-    return {(word,estimator(word.lower()),tag)
-        for word,tag in tags if is_content_word(word) }
-    # return {(word,haiku.estimate_syll(word), tag)
-    #         for word, tag in tags if is_content_word(word) }
-
-def basic_construtor(p_words):
-    out = []
-    for x in [5,7,5]:
-        line1 = []
-        line1s = 0
-        syl = x
-        v_used = False
-        while line1s != syl:
-            c_word = p_words.pop()
-            if line1s + c_word[1] <= syl and (c_word[2] == 'VERB') != v_used:
-                if c_word[2] == 'VERB':
-                    v_used = True
-                line1.append(c_word[0])
-                line1s +=c_word[1]
-            else:
-                p_words.add(c_word)
-        print(*line1)
-        out.append(line1)
-
-
 if __name__ == "__main__":
+    # creates and trains tagger
     tagger = Tagger()
     tagger_train(tagger)
-    cs = cat(n_brown.words(categories="news"),tagger)
-    print(cs)
-    basic_construtor(cs)
+
+    # applies tags to given word set (default is Brown "News")
+    source = n_brown.words(categories="news")
+    tags = tagger.tag(source)
+
+    # creates haikus usuing the created tags, default number is 1 if no number is specified.
+    haiku = haiku.arrange_haiku(tags, 5)
+    for h in haiku:
+        print(h)
+        print()
