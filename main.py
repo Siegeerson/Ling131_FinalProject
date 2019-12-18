29379b2555c9a4a2f81fd093bbcacc67c055373d
Benjamin Siege <ben.siege@gmail.com>
2019-12-17 19:54:50 -0500

    Added external source
    Allowed for external text to be used as as source for haiku.
    Try:
    python main.py --custom-text lorem_test.txt
    and
    python main.py --custom-text raven.txt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Main file to run.
 import re
 import nltk
 import haiku
+import os
+import argparse
 from nltk.corpus import PlaintextCorpusReader
 from nltk.corpus import brown as n_brown
 from nltk.corpus import wordnet as wn
@@ -16,34 +18,26 @@ def is_content_word(word):
     """A content word is not on the stoplist and its first character is a letter."""
     return word.lower() not in STOPLIST and word[0].isalpha()
 
-# def tagger_train(a):
-#     """
-#     Trains a tagger based on the brown news corpus
-#     :param a: a tagger to train
-#     :return:
-#     """
-#     sents = n_brown.tagged_sents(categories="fiction",tagset="universal")
-#     sents_train = sents[0:int(len(sents) * .9)]
-#     a.train(sents_train)
 
-#
-# def make_haikus(source, n):
-#     tagger = Tagger()
-#     tagger_train(tagger)
-#     tags = tagger.tag(source)
+def make_haikus(source):
+    tagger = Tagger(n_brown.tagged_sents(categories="fiction",tagset="universal"))
+    tags = tagger.tag(source)
+    return haiku.arrange_haiku(tags,4)
 
 
 if __name__ == "__main__":
     # creates and trains tagger
-    tagger = Tagger(n_brown.tagged_sents(categories="fiction",tagset="universal"))
-    # tagger_train(tagger)
-
-    # applies tags to given word set (default is Brown "News")
-    source = n_brown.words(categories="news")
-    tags = tagger.tag(source)
-
-    # creates haikus usuing the created tags, default number is 1 if no number is specified.
-    haiku = haiku.arrange_haiku(tags, 5)
-    for h in haiku:
-        print(h)
-        print()
+    parser = argparse.ArgumentParser(description="Haiku generator")
+    parser.add_argument("--custom-text")
+    args = vars(parser.parse_args())
+    if args.get('custom_text',None):
+        path = args['custom_text']
+        if os.path.isfile(path):
+            text = ""
+            with open(path,"r") as input:
+                text = input.read()
+            haikus = make_haikus(nltk.word_tokenize(text))
+            print(*haikus,sep="\n")
+    else:
+        haikus = make_haikus(n_brown.words(categories="news"))
+        print(*haikus,sep="\n")
