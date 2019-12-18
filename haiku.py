c3586aecf772fc9aa7c6ebcd27c51f997c50e586
Benjamin Siege <ben.siege@gmail.com>
2019-12-18 12:07:08 -0500

    Code cleanup
    Cleaned up code using IDE & PEP8 and added gitignore for junk added by pycharm

--- haiku.py
+++ haiku.py
@@ -5,23 +5,23 @@ Our haiku generator
 import random
 
 # Vowels, used to identify syllables
-VOWELS = ['a','e','i','o','u','y']
+VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
 # Parts of Speach which our haiku items can take
 TAGS = ['ADJ', 'ADV', 'DET', 'NOUN', 'NN', 'NUM', 'VERB', 'ADP', 'PRT', 'CONJ']
 # Transitions to make as close to a cohesive thought as possible
 SENTENCE = {
-            'S'     : ['DET', 'NUM', 'ADJ', 'NOUN', 'NN'],
-            'DET'   : ['NUM', 'ADJ', 'NOUN', 'NN'],
-            'NUM'   : ['ADJ', 'NOUN', 'NN'],
-            'ADJ'   : ['NOUN', 'NN'],
-            'NOUN'  : ['ADP', 'PRT', 'VERB'],
-            'NN'    : ['ADP', 'PRT', 'VERB'],
-            'ADP'   : ['DET', 'NUM', 'NOUN', 'NN'],
-            'PRT'   : ['DET', 'NUM', 'NOUN', 'NN'],
-            'VERB'  : ['ADV', 'ADP', 'PRT'],
-            'ADV'   : ['CONJ', 'ADP', 'PRT'],
-            'CONJ'  : ['DET', 'NUM', 'ADJ', 'NOUN', 'NN']
-            }
+    'S': ['DET', 'NUM', 'ADJ', 'NOUN', 'NN'],
+    'DET': ['NUM', 'ADJ', 'NOUN', 'NN'],
+    'NUM': ['ADJ', 'NOUN', 'NN'],
+    'ADJ': ['NOUN', 'NN'],
+    'NOUN': ['ADP', 'PRT', 'VERB'],
+    'NN': ['ADP', 'PRT', 'VERB'],
+    'ADP': ['DET', 'NUM', 'NOUN', 'NN'],
+    'PRT': ['DET', 'NUM', 'NOUN', 'NN'],
+    'VERB': ['ADV', 'ADP', 'PRT'],
+    'ADV': ['CONJ', 'ADP', 'PRT'],
+    'CONJ': ['DET', 'NUM', 'ADJ', 'NOUN', 'NN']
+}
 
 
 def estimate_syll(word):
@@ -31,31 +31,33 @@ def estimate_syll(word):
     :param word: string of a word
     :return: int guess to number of syllables
     """
-    wordL = word.lower()
-    w = list(wordL)
+    word_l = word.lower()
+    w = list(word_l)
     syll = 0
     for i in range(0, len(w)):
-        cur = is_syll(wordL, i, syll)
+        cur = is_syll(word_l, i, syll)
         # print("{} is syllable: {}".format(w[i], cur))
         if cur:
             syll += 1
 
-    #Checks the special cases of the word ending in 'es' or in 'ed'
+    # Checks the special cases of the word ending in 'es' or in 'ed'
     # checks previous letter wasn't vowel because if it was, the 'es' wouldn't have been assigned a syllable
-    if len(wordL) > 2 and wordL[-2:] == 'es':
-        if (wordL[-3] not in VOWELS) and (wordL[-3] not in ['x', 'c', 's', 'z', 'h', 'g']):
+    if len(word_l) > 2 and word_l[-2:] == 'es':
+        if (word_l[-3] not in VOWELS) and (word_l[-3] not in ['x', 'c', 's', 'z', 'h', 'g']):
             syll -= 1
 
-    if len(wordL) > 2 and wordL[-2:] == 'ed':
-        if (wordL[-3] not in VOWELS) and (wordL[-3] in ['n', 'm', 'k', 'g','s', 'z', 'h', 'w']):
+    if len(word_l) > 2 and word_l[-2:] == 'ed':
+        if (word_l[-3] not in VOWELS) and (word_l[-3] in ['n', 'm', 'k', 'g', 's', 'z', 'h', 'w']):
             syll -= 1
 
     return syll
+
 
 def is_syll(word, i, syll):
     """
     decides if a letter is a new syllable in a word
 
+    :param syll:
     :param word: whole word (string)
     :param i: index of current letter to be examined
     :return: true if is syllable, false if not
@@ -69,14 +71,16 @@ def is_syll(word, i, syll):
 
     return False
 
+
 def prev_is_syll(word, i, syll):
     """
     checks if the current letter is a second (or third) vowel in a single syllable
     """
     if syll > 0 and i > 0:
-        if word[i-1] in VOWELS and not is_qu(word, i-1):
+        if word[i - 1] in VOWELS and not is_qu(word, i - 1):
             return True
     return False
+
 
 def is_silent_e(word, i, syll):
     """
@@ -85,13 +89,15 @@ def is_silent_e(word, i, syll):
     """
     return (word[i] == 'e' and i + 1 == len(word)) and syll != 0
 
+
 def is_qu(word, i):
     """
     checks if the given vowel is a 'u' after a 'q', because that should have a vowel.
     """
-    return i > 0 and (word[i] == 'u' and word[i-1] == 'q')
+    return i > 0 and (word[i] == 'u' and word[i - 1] == 'q')
 
-def arrange_haiku(words_tags, num_haiku = 1):
+
+def arrange_haiku(words_tags, num_haiku=1):
     """
 
     :param words_tags: tuple with (word, tag)
@@ -107,7 +113,7 @@ def arrange_haiku(words_tags, num_haiku = 1):
         cur = 'S'
         haiku = ""
         # first line has 5 syllables, second has 7, third has 5
-        for x in [5,7,5]:
+        for x in [5, 7, 5]:
             line = []
             linesyl = 0
             # while there need to be more syllables in the line
@@ -115,7 +121,8 @@ def arrange_haiku(words_tags, num_haiku = 1):
                 types = SENTENCE[cur][:]
                 options = []
                 new_cur = ""
-                # until you have some words to pick from which won't overload the syllable needs and are of a compatible next POS
+                # until you have some words to pick from which won't overload the syllable needs and are of a 
+                # compatible next POS 
                 while len(options) < 1 and len(types) > 0:
                     new_cur = random.choice(types)
                     options = [entry for entry in info if entry[1] == new_cur if entry[2] <= (x - linesyl)]
--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 Main file to run.
 
 """
-import re
-import nltk
-import haiku
-import os
-import string
 import argparse
+import os
 import random
-from nltk.corpus import PlaintextCorpusReader
+import string
+
+import nltk
 from nltk.corpus import brown as n_brown
-from nltk.corpus import wordnet as wn
+
+import haiku
 from postag import Tagger
 
 STOPLIST = set(nltk.corpus.stopwords.words())
+
 
 def is_content_word(word):
     """A content word is not on the stoplist and its first character is a letter."""
@@ -22,31 +22,33 @@ def is_content_word(word):
 
 
 def make_haikus(source):
-    tagger = Tagger(n_brown.tagged_sents(categories="fiction",tagset="universal"))
+    tagger = Tagger(n_brown.tagged_sents(categories="fiction", tagset="universal"))
     tags = tagger.tag(source)
-    return haiku.arrange_haiku(tags,4)
+    return haiku.arrange_haiku(tags, 4)
+
 
 def random_generator(size=4, chars=string.ascii_uppercase + string.digits):
-     return ''.join(random.choice(chars) for x in range(size))
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 if __name__ == "__main__":
     # creates and trains tagger
     parser = argparse.ArgumentParser(description="Haiku generator")
     parser.add_argument("--custom-text")
-    parser.add_argument("--store",action="store_true")
+    parser.add_argument("--store", action="store_true")
     args = vars(parser.parse_args())
-    if args.get('custom_text',None):
+    haikus = []
+    if args.get('custom_text', None):
         path = args['custom_text']
         if os.path.isfile(path):
-            text = ""
-            with open(path,"r") as input:
+            with open(path, "r") as input:
                 text = input.read()
             haikus = make_haikus(nltk.word_tokenize(text))
-            print(*haikus,sep="\n")
+            print(*haikus, sep="\n")
     else:
         haikus = make_haikus(n_brown.words(categories="news"))
-        print(*haikus,sep="\n")
-    if args.get("store",None):
-        file_name = "generated_poems\\haikus_"+random_generator()+".txt"
+        print(*haikus, sep="\n")
+    if args.get("store", None):
+        file_name = "generated_poems\\haikus_" + random_generator() + ".txt"
         with open(file_name, "w") as output:
             output.write("\n".join(haikus))
